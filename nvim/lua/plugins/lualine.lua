1a3536204e0d0e0d91bb7e0ff3ba0e2227dc34ef
CalvinLe08 <lenguyenkhang102002@gmail.com>
2025-05-14 15:19:13 +0700

    neovim: change lualine separator

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,50 +1,67 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
 
-    config = function()
-      require("lualine").setup({
-        options = {
-          icons_enabled = true,
-          theme = "Dracula",
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          always_show_tabline = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 100,
-            tabline = 100,
-            winbar = 100,
-          },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { "filename" },
-          lualine_x = { "encoding", "fileformat", "filetype" },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {},
-      })
-    end,
-  },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    icons_enabled = true,
+                    theme = "Dracula",
+                    section_separators = { left = '', right = '' },
+                    component_separators = { left = '', right = '' },
+                    disabled_filetypes = {
+                        statusline = {},
+                        winbar = {},
+                    },
+                    ignore_focus = {},
+                    always_divide_middle = true,
+                    always_show_tabline = true,
+                    globalstatus = false,
+                    refresh = {
+                        statusline = 100,
+                        tabline = 100,
+                        winbar = 100,
+                    },
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = { "branch", "diff", "diagnostics" },
+                    lualine_c = {
+                        {
+                            "filename",
+                            path = 1,
+
+                            file_status = true,     -- Displays file status (readonly status, modified status)
+                            newfile_status = false, -- Display new file status (new file means no write after created)
+
+                            shorting_target = 40,   -- Shortens path to leave 40 spaces in the window
+                            -- for other components. (terrible name, any suggestions?)
+                            symbols = {
+                                modified = '[+]',      -- Text to show when the file is modified.
+                                readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+                                unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                                newfile = '[New]',     -- Text to show for newly created file before first write
+                            }
+                        }
+                    },
+                    lualine_x = { "encoding", "fileformat", "filetype" },
+                    lualine_y = { "progress" },
+                    lualine_z = { "location" },
+                },
+                inactive_sections = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = { "filename" },
+                    lualine_x = { "location" },
+                    lualine_y = {},
+                    lualine_z = {},
+                },
+                tabline = {},
+                winbar = {},
+                inactive_winbar = {},
+                extensions = {},
+            })
+        end,
+    },
 }
