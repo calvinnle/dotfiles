7dd63565bca405c23a3894143e182af19532c893
CalvinLe08 <lenguyenkhang102002@gmail.com>
2025-11-29 15:56:39 +0700

    update lsp for completions

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -18,16 +18,53 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+		lazy = false,
 		config = function()
-			vim.lsp.enable("lua_ls")
-			vim.lsp.config("clangd", {
-				cmd = {
-					"clangd",
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+			-- lua
+			vim.lsp.config["lua_ls"] = {
+				cmd = { "lua-language-server" },
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = { enable = false },
+					},
 				},
+			}
+
+			vim.lsp.enable("lua_ls")
+
+			vim.lsp.config["gopls"] = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.config["clang_d"] = {
+				capabilities = capabilities,
+			}
+
+			-- python
+			vim.lsp.config["pyright"] = {
+				capabilities = capabilities,
+			}
+
+			vim.lsp.enable({
+				"gopls",
+				"pyright",
+				"clang_d",
 			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
 	},
