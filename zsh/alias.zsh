c4ccff65421d99f7729cfbcb1434d3c08233e2a8
CalvinLe08 <lenguyenkhang102002@gmail.com>
2025-12-15 14:16:15 +0700

    zsh: delete unnecessary aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,11 +1,9 @@
 # alias ls=lsPretty
 # alias ll=llPretty
 alias wireshark="open /Applications/Wireshark.app/"
-alias cat="bat"
 alias v="nvim"
 alias g++="g++ -std=c++17"
 alias cpf="bash /Users/lenguyenkhang/dotfiles/helper/copyfile.sh"
 alias f="fastfetch"
 alias python="python3"
-alias i3="bash ~/dotfiles/scripts/start_i3.sh"
-alias stop-i3="bash ~/dotfiles/scripts/end_i3.sh"
+alias ls="ls --color=never"
